eeb2fbd088fc08845a612fd4a5d68bc677fd4f77
NokeyWang <imnokey@sina.com>
2015-10-23 23:39:41 +0800

    教师职称只能为"讲师"、"副教授"或"教授"

---
+++ src/main/java/util/TeacherTitleException.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * Created by CHester on 15/10/23.
+ */
+public class TeacherTitleException extends Exception {
+
+    public TeacherTitleException() {
+    }
+
+    public TeacherTitleException(String message) {
+        super(message);
+    }
+}
