7e78a703989205b9fb556590608cc249d5692d81
NokeyWang <imnokey@sina.com>
2015-10-23 23:38:54 +0800

    学生年级必须为2012~2015

---
+++ src/main/java/util/StudentGradeException.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * Created by CHester on 15/10/23.
+ */
+public class StudentGradeException extends Exception {
+
+    public StudentGradeException() {
+    }
+
+    public StudentGradeException(String message) {
+        super(message);
+    }
+}
